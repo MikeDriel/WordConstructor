ad551529fe066abac5dd845bf5057cdb4bcbc707
MikeDriel <94164663+MikeDriel@users.noreply.github.com>
2023-05-16 14:24:40 +0200

    Resource leak fix

--- src/WordConstructor.java
+++ src/WordConstructor.java
@@ -8,30 +8,31 @@ public class WordConstructor {
         String currentLetter;
         int j = 0;
 
-        //user input
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a word: " + "\n");
-        String currentWord = scanner.nextLine();
-        System.out.println("\n" + "The word is: " + currentWord + "\n");
-        sleep(1000);
+        try (//user input
+        Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a word: " + "\n");
+            String currentWord = scanner.nextLine();
+            System.out.println("\n" + "The word is: " + currentWord + "\n");
+            sleep(1000);
 
 
-        //loop through the word
-        while (foundLetters.length() < currentWord.length()) {
-            for (int i = 0; i < alphabet.length; i++) {
-                if (alphabet[i] != currentWord.charAt(j)) {
-                    sleep(100);
-                    System.out.println(foundLetters + alphabet[i]);
-                } else {
-                    currentLetter = String.valueOf(alphabet[i]);
-                    foundLetters += alphabet[i];
-                    i = -1;
-                    System.out.println("Letter found: " + currentLetter);
-                    if (foundLetters.length() == currentWord.length()) {
-                        System.out.println("\n" + "Word found: " + foundLetters);
-                        break;
+            //loop through the word
+            while (foundLetters.length() < currentWord.length()) {
+                for (int i = 0; i < alphabet.length; i++) {
+                    if (alphabet[i] != currentWord.charAt(j)) {
+                        sleep(100);
+                        System.out.println(foundLetters + alphabet[i]);
+                    } else {
+                        currentLetter = String.valueOf(alphabet[i]);
+                        foundLetters += alphabet[i];
+                        i = -1;
+                        System.out.println("Letter found: " + currentLetter);
+                        if (foundLetters.length() == currentWord.length()) {
+                            System.out.println("\n" + "Word found: " + foundLetters);
+                            break;
+                        }
+                        j++;
                     }
-                    j++;
                 }
             }
         }
